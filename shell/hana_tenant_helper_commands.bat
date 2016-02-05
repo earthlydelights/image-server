5bc855a4ee044407fcf05785fa660b9ccc03cacc
cthiebaud <christophe.thiebaud@aequologica.net>
2016-02-05 03:37:34 +0100

    memorized pwds

--- shell/hana_tenant_helper_commands.bat
+++ shell/hana_tenant_helper_commands.bat
@@ -15,7 +15,8 @@ rem 	\c  -n localhost:30015 -u system
 rem 	create user bigchief password USUAL_PASSWORD
 rem 	grant content_admin to bigchief with admin option
 rem ./neo.sh bind-db -h hanatrial.ondemand.com -a p1630844092trial -b imageserver -i imageserverdb --db-user bigchief --db-password USUAL_PASSWORD# -u p1630844092 -p USUAL_PASSWORD
-rem bigchief 1941866044 = USUAL_PASSWORD
+rem system 1630844092 = USUAL_PASSWORD
+rem bigchief 1630844092 = USUAL_PASSWORD#
 
 rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem 
 
@@ -28,7 +29,8 @@ rem 	\c  -n localhost:30015 -u system
 rem 	create user bigchief password USUAL_PASSWORD
 rem 	grant content_admin to bigchief with admin option
 rem ./neo.sh bind-db -h hanatrial.ondemand.com -a p1941866044trial -b imageserver -i imageserverdb --db-user bigchief --db-password USUAL_PASSWORD -u p1941866044 -p USUAL_PASSWORD
-rem bigchief 1941866044 = USUAL_PASSWORD
+rem system 1941866044 = USUAL_PASSWORD
+rem bigchief 1941866044 = USUAL_PASSWORD#
 
 rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem 
 
@@ -41,5 +43,6 @@ rem 	\c  -n localhost:30015 -u system
 rem 	create user bigchief password USUAL_PASSWORD
 rem 	grant content_admin to bigchief with admin option
 rem ./neo.sh bind-db -h hanatrial.ondemand.com -a p1941872433trial -b imageserver -i imageserverdb --db-user bigchief --db-password USUAL_PASSWORD% -u p1941872433 -p USUAL_PASSWORD
+rem system 1941872433 = USUAL_PASSWORD
 rem bigchief 1941872433 = USUAL_PASSWORD%
 

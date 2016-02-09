f894bcabd60c3058e9b2ff571062ebe73b2b9d74
cthiebaud <christophe.thiebaud@aequologica.net>
2016-02-09 20:53:46 +0100

    moved getEntityManagerFactory utility to geppaequo
    + added shakuntala config

--- src/main/java/garden/delights/earthly/imageserver/persistence/Persistor.java
+++ src/main/java/garden/delights/earthly/imageserver/persistence/Persistor.java
@@ -1,31 +1,28 @@
 package garden.delights.earthly.imageserver.persistence;
 
+import static net.aequologica.neo.geppaequo.persistence.DbUtils.getEntityManagerFactory;
+
 import java.io.Closeable;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import javax.naming.InitialContext;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.servlet.ServletException;
-import javax.sql.DataSource;
 
-import org.eclipse.persistence.config.PersistenceUnitProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Persistor implements Closeable {
 
+    @SuppressWarnings("unused")
     private final static Logger log = LoggerFactory.getLogger(Persistor.class);
     
     final private EntityManagerFactory emf;
     
     public Persistor() {
-        this.emf = getEntityManagerFactory();
+        this.emf = getEntityManagerFactory("buildhubPersist");
         
         Runtime.getRuntime().addShutdownHook(new Thread() {
             @Override
@@ -99,50 +96,4 @@ public class Persistor implements Closeable {
 
     
     
-    static private EntityManagerFactory getEntityManagerFactory() {
-
-        // try remote
-        {
-            EntityManagerFactory emfRemote  = null;
-            EntityManager        em         = null;
-            try {
-                InitialContext      ctx         = new InitialContext();
-                DataSource          ds          = (DataSource)ctx.lookup("java:comp/env/jdbc/DefaultDB");
-                Map<Object, Object> properties  = new HashMap<>();
-                
-                properties.put(PersistenceUnitProperties.NON_JTA_DATASOURCE, ds);
-                emfRemote = Persistence.createEntityManagerFactory("image-server", properties);
-
-                em = emfRemote.createEntityManager();
-                
-                return emfRemote;
-            } catch (Exception e) {
-                log.error("emfRemote.createEntityManager threw this exception and I ignored it" , e);
-            } finally {
-                if (em != null) {
-                    em.close();
-                }
-            }
-        }
-
-        // try local
-        {
-            EntityManagerFactory    emfLocal    = null;
-            EntityManager           em          = null;
-            try {
-                emfLocal = Persistence.createEntityManagerFactory("image-server-local");
-                em = emfLocal.createEntityManager();
-                return emfLocal;
-            } catch (Exception e) {
-                log.error("emfLocal.createEntityManager threw this exception and I ignored it" , e);
-            } finally {
-                if (em != null) {
-                    em.close();
-                }
-            }
-        }
-
-       return null;
-    }
-
 }

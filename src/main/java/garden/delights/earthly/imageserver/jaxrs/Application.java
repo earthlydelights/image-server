86239bb499d40271dd983eed62702158eddb89ce
cthiebaud <christophe.thiebaud@aequologica.net>
2016-06-28 16:43:13 +0200

    LoggingFeature

--- src/main/java/garden/delights/earthly/imageserver/jaxrs/Application.java
+++ src/main/java/garden/delights/earthly/imageserver/jaxrs/Application.java
@@ -2,7 +2,7 @@ package garden.delights.earthly.imageserver.jaxrs;
 
 import javax.ws.rs.ApplicationPath;
 
-import org.glassfish.jersey.filter.LoggingFilter;
+import org.glassfish.jersey.logging.LoggingFeature;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.mvc.jsp.JspMvcFeature;
@@ -14,7 +14,7 @@ public class Application extends ResourceConfig {
 
     public Application() {
 
-        register(LoggingFilter.class);
+        register(LoggingFeature.class);
         register(JspMvcFeature.class);
         register(MultiPartFeature.class);
         register(JacksonJsonProvider.class);
